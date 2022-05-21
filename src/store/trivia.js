42abbe74af69b6e617c342dc8cf696c818745c98
FloValdes <ftvaldes1@uc.cl>
2022-05-21 19:57:54 -0400

    feat(trivia-store): basic store setup

---
+++ src/store/trivia.js
@@ -0,0 +1,18 @@
+const initialState = {
+  questions: [],
+  currentIndex: 0,
+  isPlaying: false,
+};
+const getters = {
+};
+const actions = {
+};
+const mutations = {
+};
+
+export default {
+  state: { ...initialState },
+  getters,
+  actions,
+  mutations,
+};
