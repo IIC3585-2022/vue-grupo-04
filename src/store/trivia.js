895983bba7b9cddce99f79135989c076155bfade
FloValdes <ftvaldes1@uc.cl>
2022-05-21 20:14:48 -0400

    feat(TriviaView): add main trivia view

--- src/store/trivia.js
+++ src/store/trivia.js
@@ -1,3 +1,5 @@
+import triviaApi from '../api/trivia';
+
 const initialState = {
   questions: [],
   currentIndex: 0,
@@ -5,9 +7,28 @@ const initialState = {
 };
 const getters = {
 };
+
 const actions = {
+  async updateQuestions({ commit }, params) {
+    const questions = await triviaApi.getQuestions(params);
+    commit('setQuestions', questions.data.results);
+    commit('setIsPlaying', true);
+  },
+  nextQuestion({ commit, state }) {
+    commit('setCurrentIndex', state.currentIndex + 1);
+  },
 };
+
 const mutations = {
+  setQuestions(state, payload) {
+    state.questions = payload;
+  },
+  setCurrentIndex(state, payload) {
+    state.currentIndex = payload;
+  },
+  setIsPlaying(state, payload) {
+    state.isPlaying = payload;
+  },
 };
 
 export default {
