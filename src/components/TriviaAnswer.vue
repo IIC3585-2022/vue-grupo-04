a51261ff180b9f733dd86daf850e2f5480695aa4
FloValdes <ftvaldes1@uc.cl>
2022-05-21 20:20:15 -0400

    feat(trivia-answer): add trivia answer component

---
+++ src/components/TriviaAnswer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button
+      class="p-4 text-xl bg-green-400 my-3 w-5/6"
+      @click="$emit('answer', answer.correct)"
+    >
+      {{ answer.value }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps(['answer']);
+</script>
